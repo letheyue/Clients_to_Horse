82e132e455a94957b199b50aa642de22d1ba61ba
letheyue <sadameyy@hotmail.com>
2017-03-19 22:28:09 -0500

    Sign Up 1.0

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
 
+  resources :users
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'application#hello'
