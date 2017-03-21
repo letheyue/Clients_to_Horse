9827b03b026ee2ee96462c9d84fbcb3ab8d0675b
letheyue <sadameyy@hotmail.com>
2017-03-20 21:26:02 -0500

    Iteration 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,10 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # # and use secure cookies.
+  config.force_ssl = true
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
