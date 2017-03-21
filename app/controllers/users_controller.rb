643889b7bf8b7b21be7ce74d64ea5474cf65a51c
letheyue <sadameyy@hotmail.com>
2017-03-20 21:16:13 -0500

    Iteration 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Clients to Horse App!"
+      redirect_to @user
     else
       render 'new'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
 
+  post '/signup', to: 'users#create'
+
   resources :users
 
 
