702ffe07679fdf79b7eefb1def04259f7a2be564
letheyue <sadameyy@hotmail.com>
2017-03-26 15:59:16 -0500

    update seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170326193542) do
-ActiveRecord::Schema.define(version: 20170326182915) do
 
   create_table "horses", force: :cascade do |t|
     t.string   "name"
@@ -58,6 +57,5 @@ ActiveRecord::Schema.define(version: 20170326182915) do
     t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-end
 
 end
