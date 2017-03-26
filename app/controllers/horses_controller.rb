c4c28b2a8048560a0b401024e8b993be135f8724
Jean <jency267@tamu.edu>
2017-03-26 19:55:23 +0000

    minor adjust

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -7,6 +7,7 @@ class HorsesController < ApplicationController
   def show
     id = params[:id] # retrieve movie ID from URI route
     @horse = Horse.find(id) # look up movie by unique ID
+    @owner = Owner.find(@horse.owner_id)
     # will render app/views/movies/show.<extension> by default
   end
 
