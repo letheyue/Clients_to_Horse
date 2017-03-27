5c8c35fe1a14092185fb86d7052731386fb116d5
letheyue <sadameyy@hotmail.com>
2017-03-27 15:25:12 -0500

    Search Function 1.2

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -3,6 +3,6 @@ class Horse < ApplicationRecord
   has_many :activities, through: :horse_activities
 
   def self.search(search)
-    where("name LIKE ? ", "%#{search}%")
+    where("name LIKE ?  OR age LIKE ? OR sex LIKE ? OR breed LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%","%#{search}%")
   end
 end 
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,6 +2,6 @@ class Owner < ApplicationRecord
     has_many :horses
 
     def self.search(search)
-        where("name LIKE ? ", "%#{search}%")
+        where("name LIKE ? OR address LIKE ? OR email LIKE ? OR comments LIKE ? ", "%#{search}", "%#{search}", "%#{search}", "%#{search}%")
     end
 end
