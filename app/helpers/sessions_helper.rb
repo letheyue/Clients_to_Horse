bb30a1dbe51dbfa05e0482c4cb08b5b7662b2c81
letheyue <sadameyy@hotmail.com>
2017-03-26 21:38:32 -0500

    Log In 1.3

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,8 +4,21 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   #if the user has been logged in, return ture, otherwise, return false.
@@ -13,7 +26,21 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # remember the user's status even close the browser.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,11 @@ class User < ApplicationRecord
 
   # if the roken is equal to digest, return ture
   def authenicated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
