aa07f7a6cd1a4a215642243f126e0f367de2b5f6
letheyue <sadameyy@hotmail.com>
2017-04-09 23:30:12 -0500

    debug calendar

--- app/helpers/calendars_helper.rb
+++ app/helpers/calendars_helper.rb
@@ -1,4 +1,4 @@
-module CalendarHelper
+module CalendarsHelper
   def calendar(date = Date.today, & block)
     Calendar.new(self, date, block).table
   end
--- test/controllers/calendars_controller_test.rb
+++ test/controllers/calendars_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CalendarControllerTest < ActionDispatch::IntegrationTest
+class CalendarsControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
     get calendar_show_url
     assert_response :success
