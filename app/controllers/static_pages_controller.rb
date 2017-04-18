3553b3fee550b212388996ae24af372771c38119
letheyue <sadameyy@hotmail.com>
2017-04-17 23:28:00 -0500

    debug user access restriction

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,4 @@
 class StaticPagesController < ApplicationController
-  before_action :logged_in_user
 
   def home
   end
@@ -13,10 +12,4 @@ class StaticPagesController < ApplicationController
   def contact
   end
 
-  def logged_in_user
-    unless logged_in?
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
 end
