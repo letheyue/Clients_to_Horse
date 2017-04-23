d58a629d97aab50da87135629862b96cf0a53065
letheyue <sadameyy@hotmail.com>
2017-04-23 13:16:11 -0500

    rollback

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,6 @@
 ActionMailer::Base.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => "letheyuetemp.com",
     :authentication       => "plain",
     :enable_starttls_auto => true
 }
